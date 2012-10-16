92c4fa946d34aa628ca0cb845145262ce431f5e0
Jorge Jaramillo <jorgejaramillo1@gmail.com>
2012-10-16 01:52:22 -0700

    started adding user management with google accounts

--- main.py
+++ main.py
@@ -3,12 +3,15 @@
 #TODO: pagination 
 #TODO: make a way to activate/deactivate the voting without re-deploying app.
 #add user management, at least at the cookie level to allow them to take their stuff down if they want 
+#--address="1.1.1.1"
+
 import webapp2
 import jinja2
 from google.appengine.api import images
 from google.appengine.api import users
 from google.appengine.ext import db
 from google.appengine.api import memcache
+from google.appengine.api import users
 import logging
 import os
 import hashlib
@@ -17,9 +20,12 @@ import random
 
 #by default reset all cookies if the hmac is not consistent, this will allow for easily expiring cookies
 #if login is used, its better to have a different secret for non-volatile data like that.
-SECRET = "you've been trusted with this secret :)"
+SECRET = "you've been trusted with this secret :)" #+ str(random.randint(0,2000000000000))
 MEMCACHE_ART_PREFIX = "Art_"
 MEMCACHE_TOP_ART_KEY = "top"
+MEMCACHE_USER_PREFIX = "Artist_"
+MEMCACHE_USERS_KEY = "Artists"
+
 VOTING_EVENT = "1"
 VOTES_ACTIVE = True
 MAX_POSTS = 20
@@ -31,7 +37,7 @@ def getMemcacheArtID(art):
 
 def getArtByID(art_id):
     keyStr = str(art_id)
-    cache_image_key = MEMCACHE_ART_PREFIX + keyStr
+    cache_image_key = getMemcacheIDfromKey(art_id)#MEMCACHE_ART_PREFIX + keyStr
     art = None
     inMemCache = memcache.get(cache_image_key)
     if inMemCache:
@@ -71,13 +77,23 @@ class Handler(webapp2.RequestHandler):
         new_cookie_val = ""
         self.response.headers.add_header("Set-Cookie", "voted%s=%s;Path=/vote;" % (VOTING_EVENT,new_cookie_val))
         self.response.headers.add_header("Set-Cookie", "voted%s=%s;Path=/;"%  (VOTING_EVENT,new_cookie_val))
-       
+
 class Art(db.Model):
     """Art db model"""
     title = db.StringProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
     pic = db.BlobProperty()
     votes = db.IntegerProperty(default = 0)
+    artist = db.StringProperty()
+    
+class Artist(db.Model):
+    """user model """
+    nickname = db.StringProperty(required = True)
+    artistID = db.StringProperty(required = True)
+    federatedProvider = db.StringProperty(required = True)
+    arts = db.StringListProperty()
+    
+
 
 def safeMemcacheUpdate(key, val):
     client = memcache.Client()
@@ -89,8 +105,34 @@ def safeMemcacheUpdate(key, val):
         if client.cas(key, val):
             return True
         i+=1
+    logging.log('could not do memcache cas')
     return False
         
+
+def getOrCreateArtistKey(nickname, gUserID,federatedID, federatedProvider):
+    if not federatedProvider:
+        federatedProvider = "none-provided"
+    artist = None
+    
+    artistID = gUserID or federatedID
+
+    query = db.GqlQuery("SELECT __key__ FROM Artist WHERE artistID = '%s' AND federatedProvider = '%s' LIMIT 1"%(artistID, federatedProvider))
+    if query:
+        queryResult = list(query) #DESC LIMIT 10")
+        if len(queryResult) == 1:
+            artist = queryResult[0]
+    if not artist:
+        artistModel = Artist(nickname = nickname, artistID=artistID,  federatedProvider = federatedProvider)
+        artistModel.put()
+        artist = artistModel.key()
+    return artist
+
+def getOrCreateArtistKeyFromUser(user):
+    nickname = user.nickname()
+    gUserID = user.user_id()
+    federatedID = user.federated_identity()
+    federatedProvider = user.federated_provider()
+    return getOrCreateArtistKey(nickname,gUserID, federatedID, federatedProvider)
 
 def updateArtMemcache(art):
     memcacheArtID = getMemcacheArtID(art)
@@ -153,9 +195,16 @@ class GetImage(Handler):
             self.response.out.write(art.pic)
         else:
             self.response.out.write('No image')
-            
+
+    
 class MainPage(Handler):
-    def render_front(self, title="", pageHeading = PAGE_HEADING, error="", canVote = False):
+    def render_front(self,title="", pageHeading = PAGE_HEADING, error="", canVote = False,
+                     userManagement = {
+                        'user': None,
+                        'loginURL':users.create_login_url("/"),
+                        'logoutURL':users.create_logout_url("/")
+                        
+                        }):
         keys = topArts()#db.GqlQuery("SELECT * FROM Art ORDER BY created DESC LIMIT 10")
         #arts = list(arts)
         logging.info('see value in main ' + str(len(keys)))
@@ -165,12 +214,36 @@ class MainPage(Handler):
             a = getArtByID(k)
             arts.append(a)
         canVote = canVote and VOTES_ACTIVE
-        self.render("front.html", title=title,pageHeading = pageHeading,error = error, arts = arts, canVote = canVote)
+        self.render("front.html", title=title,pageHeading = pageHeading,error = error, arts = arts, canVote = canVote,userManagement=userManagement)
     
     def get(self):
         visits = 1;
         visit_cookie_str = self.request.cookies.get('visits')
-        
+        user = users.get_current_user()
+        userKey = ""
+        userManagement = {
+            'user': user,
+            'loginURL':users.create_login_url("/"),
+            'logoutURL':users.create_logout_url("/"),
+            
+            } 
+        if user:
+            userManagement['nickname'] = user.nickname() 
+            userManagement['isAdmin'] = users.is_current_user_admin();
+            userKey =  getOrCreateArtistKeyFromUser(user)
+            userManagement['userKey'] = str(userKey);
+       
+        """
+        if user:
+            greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %
+                        (user.nickname(), users.create_logout_url("/")))
+        else:
+            greeting = ("<a href=\"%s\">Sign in or register</a>." %
+                        users.create_login_url("/"))
+
+        self.response.out.write("<html><body>%s</body></html>" % greeting)
+        return
+        """    
         if visit_cookie_str:
             cookie_val = check_secure_val(str(visit_cookie_str))
             logging.info('cookie_val: %s' % cookie_val)
@@ -178,28 +251,50 @@ class MainPage(Handler):
                 visits = int(cookie_val)+1
         new_cookie_val = make_secure_val(str(visits))
         canVote = not check_secure_val(self.request.cookies.get('voted%s' % VOTING_EVENT))
+        titleAlreadyThere = self.request.get('get_title')
+        errorFromPrevRequest = self.request.get('get_error')
+        
         self.response.headers.add_header("Set-Cookie", "visits=%s" % new_cookie_val)
-        self.render_front(canVote = canVote)
+              
+        self.render_front(canVote = canVote,userManagement=userManagement, title=titleAlreadyThere, error=errorFromPrevRequest)
     def post(self):
+        title = self.request.get("title")
+        pic = self.request.get("pic")
+        user = users.get_current_user();
+
+        if not user:
+            self.redirect('/')
+            return
+
+        
+        artist = getOrCreateArtistKeyFromUser(user)
+        
+        if not artist:
+            logging.log('had some problem, could not ger artist')
+            self.redirect('/')
+        
         keys = memcache.get(MEMCACHE_TOP_ART_KEY)
         if keys and len(keys) >= MAX_POSTS:
             self.redirect('/')#only allow MAX_POSTS posts, this is going on appengine
             return
         
-        title = self.request.get("title")
-        pic = self.request.get("pic")
-        canVote = not check_secure_val(self.request.cookies.get('voted%s' % VOTING_EVENT))
+
+        #canVote = not check_secure_val(self.request.cookies.get('voted%s' % VOTING_EVENT))
+        artistKey = str(artist)
         error =""
         if len(title) > 500:
             error = "title should be less than 500 characters"
-            self.render(title,error)
-        elif title and pic:
+        if len(title) == 0:
+            error = "Your artwork needs a title"
+        canProceed = (not not title) and (not not pic) and (not error)
+        if canProceed:
             try:
                 resizedPic = images.resize(pic, 500, 300)
                 picBlob = db.Blob(resizedPic)
-                a = Art(title = title, pic=picBlob)
+                a = Art(title = title, pic=picBlob, artist=artistKey)
                 a.put()
                 topArts(True)
+                title= "";#clear title for redirect
             except images.BadImageError:
                 error = "Bad Image file"
             except images.NOT_IMAGE:
@@ -208,8 +303,18 @@ class MainPage(Handler):
                 error = "image too large, please keep it under 4MB"
             except images.Error:
                 error = "Sorry, the image you tried to upload could not be processed"
-            finally:
-                self.render_front(title=title,error = error, canVote=canVote)
+            #finally:
+                #self.render_front(title=title,error = error, canVote=canVote)
+        else:
+            error='Sorry, you need a title and a picture to submit'
+        
+        if title or error:
+            self.redirect('/?get_title=%s&get_error=%s'%(title,error))
+        else:
+            self.redirect('/')
+        
+            
+        
 
 class Remove(Handler):
     def post(self):
