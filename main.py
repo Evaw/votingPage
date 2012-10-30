07c40a1539bc27f6986b45899a85cfd5aff02db2
Jorge Jaramillo <jorgejaramillo1@gmail.com>
2012-10-30 09:51:22 -0700

    added teams

--- main.py
+++ main.py
@@ -17,37 +17,58 @@ import os
 import hashlib
 import hmac
 import random
+import re
 
 #by default reset all cookies if the hmac is not consistent, this will allow for easily expiring cookies
 #if login is used, its better to have a different secret for non-volatile data like that.
 SECRET = "you've been trusted with this secret :)" #+ str(random.randint(0,2000000000000))
 MEMCACHE_ART_PREFIX = "Art_"
+MEMCACHE_TEAM_PREFIX = "Team"
 MEMCACHE_TOP_ART_KEY = "top"
 MEMCACHE_USER_PREFIX = "Artist_"
 MEMCACHE_USERS_KEY = "Artists"
-
+MEMCACHE_TOP_TEAM_KEY = "Teams"
 VOTING_EVENT = "1"
-VOTES_ACTIVE = True
+memcache.set('app_voting_started', True) 
+VOTES_ACTIVE = memcache.get('app_voting_started') 
 MAX_POSTS = 20
 PAGE_HEADING = "Voting Page- currently max "+str(MAX_POSTS) + "posts"
-def getMemcacheIDfromKey(key):
-    return MEMCACHE_ART_PREFIX + str(key)
-def getMemcacheArtID(art):
-    return getMemcacheIDfromKey(str(art.key()))
 
+simpleStringRegex = (r'[a-zA-Z0-9]+(?:[a-zA-Z0-9 ]+[a-zA-Z0-9])*')
+
+def getMemcacheArtIDfromKey(key):
+    return MEMCACHE_ART_PREFIX + str(key)
+def getMemcacheTeamIDfromKey(key):
+    return MEMCACHE_TEAM_PREFIX + str(key)
+
+def getMemcacheArtID(art):
+    return getMemcacheArtIDfromKey(str(art.key()))
+def getMemcacheTeamID(team):
+    return getMemcacheTeamIDfromKey(str(team.key()))
 def getArtByID(art_id):
     keyStr = str(art_id)
-    cache_image_key = getMemcacheIDfromKey(art_id)#MEMCACHE_ART_PREFIX + keyStr
+    cache_image_key = getMemcacheArtIDfromKey(art_id)#MEMCACHE_ART_PREFIX + keyStr
     art = None
     inMemCache = memcache.get(cache_image_key)
     if inMemCache:
         art = inMemCache
     else:
-        logging.info('db get by id')#is this an expensive query? know gets are much cheaper writes, but is this counted on quota?
+        logging.info('db get by id art')#is this an expensive query? know gets are much cheaper writes, but is this counted on quota?
         art = db.get(keyStr)
         memcache.set(cache_image_key, art)
     return art
-
+def getTeamByID(teamID):
+    keyStr = str(teamID)
+    cache_team_key = getMemcacheTeamIDfromKey(teamID)
+    team = None
+    inMemcache = memcache.get(cache_team_key)
+    if inMemcache:
+        team = inMemcache
+    else: 
+        logging.info('db get by id team')#is this an expensive query? know gets are much cheaper writes, but is this counted on quota?
+        team = db.get(keyStr)
+        memcache.set(cache_team_key, team)
+    return team
 def hash_str(s):
     #return hashlib.sha256(s).hexdigest();
     return hmac.new(SECRET,s, hashlib.sha256).hexdigest()
@@ -85,7 +106,14 @@ class Art(db.Model):
     pic = db.BlobProperty()
     votes = db.IntegerProperty(default = 0)
     artist = db.StringProperty()
-    
+    teamKey = db.StringProperty(required = True)
+
+class Team(db.Model):
+    """team db model"""
+    name = db.StringProperty(required = True)
+    arts = db.StringListProperty();
+    created = db.DateTimeProperty(auto_now_add = True)
+
 class Artist(db.Model):
     """user model """
     nickname = db.StringProperty(required = True)
@@ -101,7 +129,7 @@ def safeMemcacheUpdate(key, val):
     tries = 100
     while i<tries:#retry loop
         counter = client.gets(key)
-        assert counter is not None, 'Uninitialized counter'
+        assert counter is not None, 'Uninitialized key'
         if client.cas(key, val):
             return True
         i+=1
@@ -127,20 +155,40 @@ def getOrCreateArtistKey(nickname, gUserID,federatedID, federatedProvider):
         artist = artistModel.key()
     return artist
 
+def getOrCreateTeamKeyFromName(name):
+    regexMatched = re.findall(simpleStringRegex, name)
+    if len(regexMatched) <=0:
+        return None
+    if regexMatched[0] != name:
+        return None
+    team = None
+    query = db.GqlQuery("SELECT __key__ FROM Team Where name = '%s'" % name)
+    if query:
+        queryResult = list(query)
+        if len(queryResult)==1:
+            team = queryResult[0]
+    if not team:
+        teamModel = Team(name = name, arts=[])
+        teamModel.put()
+        team = teamModel.key()
+    return team
 def getOrCreateArtistKeyFromUser(user):
     nickname = user.nickname()
     gUserID = user.user_id()
     federatedID = user.federated_identity()
     federatedProvider = user.federated_provider()
     return getOrCreateArtistKey(nickname,gUserID, federatedID, federatedProvider)
-
+def updateTeamMemCache(team):
+    memcacheTeamID = getMemcacheTeamID(team)
+    safeMemcacheUpdate(memcacheTeamID, team)
+    
 def updateArtMemcache(art):
     memcacheArtID = getMemcacheArtID(art)
     safeMemcacheUpdate(memcacheArtID, art)
     #updateTopMemcacheKeys(memcacheArtID, art)
     topArts(True)
-def removeFromMemcache(key):
-    memcacheArtID = getMemcacheIDfromKey(key)
+def removeFromArtMemcache(key):
+    memcacheArtID = getMemcacheArtIDfromKey(key)
     memcache.delete(memcacheArtID)
     tops = memcache.get(MEMCACHE_TOP_ART_KEY)
     topsStr = [str(a) for a in tops]
@@ -152,26 +200,77 @@ def removeFromMemcache(key):
     else:
         memcache.set(MEMCACHE_TOP_ART_KEY, tops)
 
+
+def transactionRemoveArtKeyFromTeam(teamKey, artKey):
+    team = db.get(teamKey)
+    artList = team.arts
+    if artKey in  artList:
+        artList.remove(artKey)
+    team.put()
+def removeArtFromTeamMemcache(teamKey, artKey):
+    memcacheTeamID = getMemcacheTeamIDfromKey(teamKey)
+    modifiedTeam = db.get(teamKey)
+    safeMemcacheUpdate(memcacheTeamID, modifiedTeam)
+def removeArtFromTeam(key, teamKey):
+    
+    
+    #remove model itself
+    
+    db.run_in_transaction(transactionRemoveArtKeyFromTeam,teamKey,key)
+    removeArtFromTeamMemcache(teamKey,key)
+    
 def deleteDBArtEntry(key):
     logging.info('DB transaction delete')
     db.delete(key)
     #art.delete()
     
 def deleteArt(key):
+    art = db.get(key)
+    teamKey = art.teamKey
+    #remove from other models
+    removeArtFromTeam(key, teamKey)
+    
+    #remove model itself
+    
     db.run_in_transaction(deleteDBArtEntry,key)
-    removeFromMemcache(key)
+    removeFromArtMemcache(key)
     
 def incrementDBvote(key, amount):
     art = db.get(key)
     art.votes += amount
     art.put()
 
+def addArtToTeamInTransactionHelper(teamKey, artKey):
+    team = db.get(teamKey)
+    #arts = list(team.arts)
+    
+    team.arts.append(artKey)
+    #team.arts = arts#check if we dont have to copy arts to a new list
+    team.put()
+    
+def addArtToTeam(teamKey, artKey):
+    db.run_in_transaction(addArtToTeamInTransactionHelper, teamKey, artKey)
+    team = db.get(teamKey)
+    updateTeamMemCache(team)
 def incrementVote(key, amount):
     db.run_in_transaction(incrementDBvote, key, amount)
     art = db.get(key)
     updateArtMemcache(art)
     #topArts(True)
-    
+
+def topTeams(update=False):
+    keys = memcache.get(MEMCACHE_TOP_TEAM_KEY)
+    if keys is None or update:
+        logging.info('DB query to team')
+        teams = db.GqlQuery("select * from Team ORDER BY created")
+        teams = list(teams)
+        keys = [t.key() for t in teams]
+        for t in teams:
+            if t.arts:
+                memcache.set(getMemcacheTeamID(t), t)
+        memcache.set(MEMCACHE_TOP_TEAM_KEY, keys)
+    return keys
+        
 def topArts(update=False):
     keys = memcache.get(MEMCACHE_TOP_ART_KEY)
     if keys is None or update:
@@ -208,13 +307,25 @@ class MainPage(Handler):
         keys = topArts()#db.GqlQuery("SELECT * FROM Art ORDER BY created DESC LIMIT 10")
         #arts = list(arts)
         logging.info('see value in main ' + str(len(keys)))
-        arts = []
+        arts = {}
         for k in keys:
             #self.write(repr(a.pic))
             a = getArtByID(k)
-            arts.append(a)
-        canVote = canVote and VOTES_ACTIVE
-        self.render("front.html", title=title,pageHeading = pageHeading,error = error, arts = arts, canVote = canVote,userManagement=userManagement)
+            arts[str(k)]=a
+        teamKeys = topTeams()
+        teams = []
+        for k in teamKeys :
+            t = getTeamByID(k)
+            teams.append(t)
+        canVote = canVote and memcache.get('app_voting_started')
+        """
+        for t in teams:
+            logging.info(t)
+            for aKey in t.arts:
+                logging.info(aKey)
+                logging.info(arts[aKey].pic)
+                """
+        self.render("front.html", teams=teams, title=title,pageHeading = pageHeading,error = error, arts = arts, canVote = canVote,userManagement=userManagement)
     
     def get(self):
         visits = 1;
@@ -259,8 +370,19 @@ class MainPage(Handler):
     def post(self):
         title = self.request.get("title")
         pic = self.request.get("pic")
-        user = users.get_current_user();
-
+        team = self.request.get('team')
+        user = users.get_current_user()
+        error =""
+        teamKey = None
+        if not team:
+            error = "please provide a team"
+        else:
+            teamKey = getOrCreateTeamKeyFromName(team)
+            if teamKey == None:
+                #malicious key
+                error += "bad team name"
+            else:
+                teamKey = str(teamKey)
         #if not user:
         #    self.redirect('/')
         #    return
@@ -280,35 +402,46 @@ class MainPage(Handler):
 
         #canVote = not check_secure_val(self.request.cookies.get('voted%s' % VOTING_EVENT))
         
-        error =""
+        
         if len(title) > 500:
-            error = "title should be less than 500 characters"
+            error += "title should be less than 500 characters"
         if len(title) == 0:
-            error = "Your artwork needs a title"
+            error += "Your artwork needs a title"
         canProceed = (not not title) and (not not pic) and (not error)
         if canProceed:
             try:
                 resizedPic = images.resize(pic, 500, 300)
                 picBlob = db.Blob(resizedPic)
                 if artistKey:
-                    a = Art(title = title, pic=picBlob, artist=artistKey)
+                    a = Art(title = title, pic=picBlob, artist=artistKey, teamKey = teamKey)
                 else: 
-                    a= Art(title = title, pic=picBlob) 
+                    a= Art(title = title, pic=picBlob,teamKey = teamKey) 
                 a.put()
+                team = db.get(teamKey)
+                
+                teamMemcacheID = getMemcacheTeamIDfromKey(teamKey)
+                memcache.set(teamMemcacheID, team)
+                addArtToTeam(teamKey, str(a.key()))
+                
                 topArts(True)
+                topTeams(True)
                 title= "";#clear title for redirect
             except images.BadImageError:
-                error = "Bad Image file"
+                error += "Bad Image file"
+                """
             except images.NOT_IMAGE:
-                error = "no image file"
+                error += "no image file"
             except images.IMAGE_TOO_LARGE:
-                error = "image too large, please keep it under 4MB"
+                error += "image too large, please keep it under 4MB"
+                
+            """
             except images.Error:
-                error = "Sorry, the image you tried to upload could not be processed"
+            
+                error += "Sorry, the image you tried to upload could not be processed"
             #finally:
                 #self.render_front(title=title,error = error, canVote=canVote)
         else:
-            error='Sorry, you need a title and a picture to submit'
+            error+='Sorry, you need a title and a picture to submit'
         
         if title or error:
             self.redirect('/?get_title=%s&get_error=%s'%(title,error))
@@ -337,7 +470,7 @@ class Vote(Handler):
         incrementVote(artID, 1)
         
     def post(self):
-        if not VOTES_ACTIVE:
+        if not memcache.get('app_voting_started'):
             logging.info('voting is not active, but got a request to vote, vote not counted')
             self.redirect('/')
             return
